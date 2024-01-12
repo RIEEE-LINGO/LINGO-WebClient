b42ee1260563984bd733367a7cae18a539caa94a
Mark Hills <mhills@acm.org>
2024-01-12 16:33:47 -0500

    Change dark setting on table to false

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def update_page_content(pathname):
                 *[html.Tr([html.Td(entry["Word"]), html.Td(entry["Meaning"])]) for entry in glossary_data],
             ],
             bordered=True,
-            dark=True,
+            dark=False,
             responsive=True,
             striped=True,
             hover=True,
